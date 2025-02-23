cd0758a9be4158a09443e34beef8a42e8d285534
Your Name <you@example.com>
2025-02-23 23:33:30 +0500

    Добавьте файлы проекта.

---
+++ Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("1.start");
+        Console.WriteLine("2.kill");
+
+    }
+}
